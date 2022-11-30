43e1103062774ed676ed9d153aad1ee23eb41dd5
Dcole202 <dcole202ts@gmail.com>
2022-11-30 15:45:30 -0500

    Index Pages/Index CSS

---
+++ OldPersonsHome/resources/views/adminIndex.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="stylesheet.css">
+<link rel="stylesheet" href="adminIndex.css">
+        <header class="header">
+            <h1>Admin's Page</h1>
+        </header>
+    </head>
+    <body>
+        <div class="main">
+            <div>
+            <br>
+            <form action="" method="POST">
+    
+                <div class="flex-container">
+                    <div><a href="/adminReport">Admin's Report</a></div>
+                    <div><a href="/regisApproval">Regristration Approval</a></div>
+                </div>
+                <br>
+                <div class="flex-container">
+                    <div><a href="/roster">Roster</a></div>
+                    <div><a href="/newRoster">New Roster</a></div>
+                </div>
+                <br>
+                <div class="flex-container">
+                    <div><a href="/patients">Patients</a></div>
+                    <div><a href="/patientAdditionalInfo">Patient Info</a></div>
+                </div>
+                <br>
+                <div class="flex-container">
+                    <div><a href="/employee">Employees</a></div>
+                    <div><a href="/payment">Payment</a></div>
+                </div>
+                <br>
+                <div class="flex-container">
+                    <div><a href="/doctorAppt">Appointments</a></div>
+                    <div><a href="/roles">Roles</a></div>
+                </div>
+                <div class="flex-containerbtn">
+                    <div><button type="button" class="cancelbtn" name="Logout">Logout</div>
+                </div>
+            </form>
+            </div>
+        </div>
+        <footer class="footer">
+
+            </footer>
+        </body>
+        </html>
---
+++ OldPersonsHome/resources/views/patientIndex.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="stylesheet.css">
+<link rel="stylesheet" href="patientIndex.css">
+        <header class="header">
+            <h1>Patient's Page</h1>
+        </header>
+    </head>
+    <body>
+        <div class="main">
+            <div>
+            <br>
+            <form action="" method="POST">
+    
+                <div class="flex-container">
+                    <div><a href="/patientHome">Patient's Home</a></div>
+                </div>
+                <br>
+                <div class="flex-containerbtn">
+                    <div><button type="button" class="cancelbtn" name="Logout">Logout</div>
+                </div>
+            </form>
+            </div>
+        </div>
+        <footer class="footer">
+
+            </footer>
+        </body>
+        </html>
