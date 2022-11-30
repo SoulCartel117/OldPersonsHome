2a31861da2bf5de33378476e4c57781341ede3ac
Moises Manon <moisesmanonjr@hotmail.com>
2022-11-30 16:03:40 -0500

    formatted routes

--- OldPersonsHome/app/Http/Controllers/MainController.php
+++ OldPersonsHome/app/Http/Controllers/MainController.php
@@ -6,5 +6,82 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function home(){
+        return view('welcome');
+    }
+
+    public function login(){
+        return view('login');
+    }
+
+    public function regisApproval(){
+        return view('regisApproval');
+    }
+
+    public function registration(){
+        return view('registration');
+    }
+    
+    public function patientAdditionalInfo(){
+        return view('patientAdditionalInfo');
+    }
+
+    public function doctorAppt(){
+        return view('doctorAppt');
+    }
+
+    public function patientHome(){
+        return view('patientHome');
+    }
+
+    public function employee(){
+        return view('employee');
+    }
+
+    public function patients(){
+        return view('patients');
+    }
+
+    public function roster(){
+        return view('roster');
+    }
+
+    public function newRoster(){
+        return view('newRoster');
+    }
+
+    public function doctorHome(){
+        return view('doctorHome');
+    }
+
+    public function patientOfDoctor(){
+        return view('patientOfDoctor');
+    }
+
+    public function caregiverHome(){
+        return view('caregiverHome');
+    }
+
+    public function familyMemberHome(){
+        return view('familyMemberHome');
+    }
+
+    public function adminReport(){
+        return view('adminReport');
+    }
+
+    public function payment(){
+        return view('payment');
+    }
+
+    public function homepage(){
+        return view('homepage');
+    }
+
+    public function roles(){
+        return view('roles');
+    }
+
+    
+
 }
--- OldPersonsHome/routes/web.php
+++ OldPersonsHome/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,79 +14,100 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'home']);
+Route::get('/patientAdditionalInfo', [MainController::class, 'patientAdditionalInfo']);
+Route::get('/doctorAppt', [MainController::class, 'doctorAppt']);
+Route::get('/patientHome', [MainController::class, 'patientHome']);
+Route::get('/employee', [MainController::class, 'employee']);
+Route::get('/patients', [MainController::class, 'patients']);
+Route::get('/roster', [MainController::class, 'roster']);
+Route::get('/newRoster', [MainController::class, 'newRoster']);
+Route::get('/doctorHome', [MainController::class, 'doctorHome']);
+Route::get('/patientOfDoctor', [MainController::class, 'patientOfDoctor']);
+Route::get('/caregiverHome', [MainController::class, 'caregiverHome']);
+Route::get('/familyMemberHome', [MainController::class, 'familyMemberHome']);
+Route::get('/adminReport', [MainController::class, 'adminReport']);
+Route::get('/payment', [MainController::class, 'payment']);
+Route::get('/homepage', [MainController::class, 'homepage']);
+Route::get('/registration', [MainController::class, 'registration']);
+Route::get('/roles', [MainController::class, 'roles']);
+Route::get('/login', [MainController::class, 'login']);
+Route::get('/regisApproval', [MainController::class, 'regisApproval']);
 
-Route::get('/patientAdditionalInfo', function () {
-    return view('patientAdditionalInfo');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/doctorAppt', function () {
-    return view('doctorAppt');
-});
 
-Route::get('/patientHome', function () {
-    return view('patientHome');
-});
+// Route::get('/patientAdditionalInfo', function () {
+//     return view('patientAdditionalInfo');
+// });
 
-Route::get('/employee', function () {
-    return view('employee');
-});
+// Route::get('/doctorAppt', function () {
+//     return view('doctorAppt');
+// });
 
-Route::get('/patients', function () {
-    return view('patients');
-});
+// Route::get('/patientHome', function () {
+//     return view('patientHome');
+// });
 
-Route::get('/roster', function () {
-    return view('roster'); 
-});
+// Route::get('/employee', function () {
+//     return view('employee');
+// });
 
-Route::get('/newRoster', function () {
-    return view('newRoster');
-}); 
+// Route::get('/patients', function () {
+//     return view('patients');
+// });
 
-Route::get('/doctorHome', function () {
-    return view('doctorHome');
-});
+// Route::get('/roster', function () {
+//     return view('roster'); 
+// });
 
-Route::get('/patientOfDoctor', function () {
-    return view('patientOfDoctor');
-});
+// Route::get('/newRoster', function () {
+//     return view('newRoster');
+// }); 
 
-Route::get('/caregiverHome', function () {
-    return view('caregiverHome');
-});
+// Route::get('/doctorHome', function () {
+//     return view('doctorHome');
+// });
 
-Route::get('/familyMemberHome', function () {
-    return view('familyMemberHome');
-});
+// Route::get('/patientOfDoctor', function () {
+//     return view('patientOfDoctor');
+// });
 
-Route::get('/adminReport', function () {
-    return view('adminReport');
-});
+// Route::get('/caregiverHome', function () {
+//     return view('caregiverHome');
+// });
 
-Route::get('/payment', function () {
-    return view('payment');
-});
+// Route::get('/familyMemberHome', function () {
+//     return view('familyMemberHome');
+// });
 
-Route::get('/homepage', function () {
-    return view('homepage');
-});
+// Route::get('/adminReport', function () {
+//     return view('adminReport');
+// });
 
-Route::get('/registration', function () {
-    return view('registration');
-});
+// Route::get('/payment', function () {
+//     return view('payment');
+// });
 
-Route::get('/roles', function () {
-    return view('roles');
-});
+// Route::get('/homepage', function () {
+//     return view('homepage');
+// });
 
-Route::get('/login', function () {
-    return view('login');
-});
+// Route::get('/registration', function () {
+//     return view('registration');
+// });
 
-Route::get('/regisApproval', function () {
-    return view('regisApproval');
-});
+// Route::get('/roles', function () {
+//     return view('roles');
+// });
+
+// Route::get('/login', function () {
+//     return view('login');
+// });
+
+// Route::get('/regisApproval', function () {
+//     return view('regisApproval');
+// });
 
