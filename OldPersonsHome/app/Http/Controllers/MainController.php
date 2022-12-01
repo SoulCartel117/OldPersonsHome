762194368ff032fda43734fabf14b8eb4b85db7a
SoulCartel117 <jeremiahmartin117@gmail.com>
2022-12-01 15:37:00 -0500

    registration includes EM information

--- OldPersonsHome/app/Http/Controllers/MainController.php
+++ OldPersonsHome/app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\accounts;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Symfony\Contracts\Service\Attribute\Required;
 
@@ -15,7 +16,7 @@ class MainController extends Controller
     }
 
     public function login(){
-        return view('login');
+        return view('login', ['loginError'=>'Please log in']);
     }
 
     public function regisApproval(){
@@ -106,6 +107,10 @@ class MainController extends Controller
         return view('famIndex');
     }
 
+    public function registrationGet(){
+        return view('registration');
+    }
+
     
     // registration
     public function registration(Request $request){
@@ -125,6 +130,28 @@ class MainController extends Controller
             'DOB' => $request->input('DOB')
         ]);
 
+        $user = DB::table('accounts')->where
+            ('Email', $request->input('email'))->first();
+
+        DB::table('familycode')->insert([
+            'patientID'=>$user->ID,
+            'familyCode' => $request->input('familyCode')
+        ]);
+
+        $FCID = DB::table('familycode')->where
+        ('patientID', $user->ID)->first();
+        
+        if($request->input('role') == 5){
+            DB::table('patient')->insert([
+                'patientID'=>$user->ID,
+                'FCID' => $FCID->FCID,
+                'admissionDate' => (date('Y')."-".date('m')."-".date('d')),
+                'emContact' => $request->input('familyName'),
+                'emContactPhNo' => $request->input('familyPhone'),
+                'relationEmContact' => $request->input('familyRelation')
+            ]);
+        }
+
         // redirects to the login page
         return redirect('/login');
     }
@@ -132,31 +159,46 @@ class MainController extends Controller
 
     // Login 
     public function loginPost(Request $request){
-        // validate inputs
-        $fields = $request->validate([
-            'email' => 'required|string|unique:accounts,Email',
-            'password' => 'Required|string'
-        ]);
-
         // get user info from DB on email
         $user = DB::table('accounts')->where
-            ('Email', $fields['email'])->first();
+            ('Email', $request->input('email'))->first();
 
         // check if user has correct password
-        if(!$user || ($fields['password'] != $user->password)){
-            return response([
-                'message' => 'Your email or password is incorrect'
-            ], 401);
+        if(!$user || ($request->input('password') != $user->password)){
+            return view('login', ['loginError'=>'Your email or username is incorrect']);
         };
 
         // checks if their account is approved
-        if($user->isRegApproved = NULL){
-            return response([
-                'message' => 'Your account is not approved.'
-            ], 401);
+        if($user->isRegApproved == NULL){
+            return view('login', ['loginError'=>'Your account is not approved']);
         };
 
-        
+        // get user role
+        $role = $user->roleID;
+
+        // redirect to correct home page based on role
+        if($role == 1){
+            return redirect('/adminIndex');
+        }
+        if($role == 2){
+            return redirect('/superIndex');
+        }
+        if($role == 3){
+            return redirect('/docIndex');
+        }
+        if($role == 4){
+            return redirect('/careIndex');
+        }
+        if($role == 5){
+            return redirect('/patientHome');
+        }
+        if($role == 6){
+            return redirect('/famlyMemberHome');
+        }
+
+        // return login page if nothing else
+        return redirect('/login');
+
     }
 
 }
